25672e344d6dc755fdad16298887ec391ed9b828
TueMyHome <hoangminhduc1622@gmail.com>
2024-01-01 10:26:54 +0700

    mymy

---
+++ renderer/src/data/changelog.js
@@ -0,0 +1,17 @@
+// fixed, improved, added, progress
+export default {
+    description: "This is a small but very important update to fix some key issues!",
+    changes: [
+        {
+            title: "What's Fixed?",
+            type: "fixed",
+            items: [
+                "Spanish (LATAM) is now properly supported.",
+                "Future cases of unrecognized locales as well as locale fallback now works as intended and shouldn't cause loading issues.",
+                "Updated translations for Vietnamese locale.",
+                "Fixed an issue where certain actions (such as favoriting GIFs) caused unexpected lag.",
+                "Fixed some issues with general client lag."
+            ]
+        }
+    ]
+};
